3e9b848b07b86f775331a63d56cd65ac3d750c74
Warckut <warckut@gmail.com>
2024-05-16 15:15:45 +0500

    added bootsrap, variables, images

--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,7 @@ module.exports = {
         use: [
           "style-loader",
           "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              additionalData: '@import "/src/assets/styles/variables.scss";',
-            },
-          },
+          "sass-loader",
         ],
       },
     ],
